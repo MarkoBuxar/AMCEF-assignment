09bf18badbbe7b01c88bafdfe41f3edc0771760c
MarkoBuxar <buxarmarko@gmail.com>
2024-01-16 18:50:14 +0100

    updated packages

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
 
+import { Logger } from "./Logger/Logger";
+import { Utils } from "./Utils/Utils";
+
+Logger.Info("asd");
+Logger.Success(Utils.toJadenCase("this is a test sentence"));
